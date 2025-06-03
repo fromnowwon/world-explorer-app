59a9dfbcac9018ed474a163b228c5c16a39570c1
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-03 22:32:19 +0900

    chore: React 19 대응 패치 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import "@ant-design/v5-patch-for-react-19";
 
 export const metadata: Metadata = {
   title: "WORLD EXPLORER APP",
