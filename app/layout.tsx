3cf653bddfef2582de2c04d4069d68213567422d
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-03 21:00:23 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+
+export const metadata: Metadata = {
+  title: "WORLD EXPLORER APP",
+  description: "전 세계 국가 정보를 탐색할 수 있는 간단한 웹 앱입니다.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={`antialiased`}>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
+    </html>
+  );
+}
