5d78c9ad07bffe69efe8780f8b2d35fde0090378
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-03 23:00:30 +0900

    style: 카드 스타일 수정

--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -1,7 +1,9 @@
-import { Card } from "antd";
+import { Card, Typography } from "antd";
 import { Country } from "@/types/country";
 import Image from "next/image";
 import Link from "next/link";
+
+const { Title, Text } = Typography;
 
 interface CountryCardProps {
   country: Country;
@@ -9,18 +11,33 @@ interface CountryCardProps {
 
 export default function CountryCard({ country }: CountryCardProps) {
   return (
-    <Link href={`/country/${country.cca3}`}>
-      <Card>
-        <Image
-          src={country.flags.png}
-          alt={`Flag of ${country.name.common}`}
-          width={240}
-          height={160}
-          style={{ objectFit: "cover" }}
-        />
-        <p>수도: {country.capital?.[0] || "정보 없음"}</p>
-        <p>지역: {country.region}</p>
-        <p>인구: {country.population.toLocaleString()}명</p>
+    <Link href={`/country/${country.cca3}`} passHref>
+      <Card
+        hoverable
+        className="transition-shadow shadow-sm hover:shadow-md"
+        cover={
+          <Image
+            src={country.flags.png}
+            alt={`Flag of ${country.name.common}`}
+            width={240}
+            height={160}
+            style={{ objectFit: "cover", width: 240, height: 160 }}
+            priority
+          />
+        }
+      >
+        <Title level={5} className="mb-2">
+          {country.name.common}
+        </Title>
+        <Text type="secondary">
+          수도: {country.capital?.[0] || "정보 없음"}
+        </Text>
+        <br />
+        <Text type="secondary">지역: {country.region}</Text>
+        <br />
+        <Text type="secondary">
+          인구: {country.population.toLocaleString()}명
+        </Text>
       </Card>
     </Link>
   );
--- components/CountryList.tsx
+++ components/CountryList.tsx
@@ -6,14 +6,23 @@ import { Country } from "@/types/country";
 
 export default function CountryList({ countries }: { countries: Country[] }) {
   return (
-    <List
-      grid={{ gutter: 16, column: 4 }}
-      dataSource={countries}
-      renderItem={(country) => (
-        <List.Item>
-          <CountryCard country={country} />
-        </List.Item>
-      )}
-    />
+    <div className="p-8">
+      <List
+        grid={{
+          gutter: 24,
+          xs: 1,
+          sm: 2,
+          md: 3,
+          lg: 4,
+          xl: 5,
+        }}
+        dataSource={countries}
+        renderItem={(country) => (
+          <List.Item>
+            <CountryCard country={country} />
+          </List.Item>
+        )}
+      />
+    </div>
   );
 }
