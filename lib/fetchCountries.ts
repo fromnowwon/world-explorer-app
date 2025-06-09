2607c632f58489c9d15b075ce80dff3b49d7676e
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-09 16:46:28 +0900

    fix: API 업데이트 반영

--- lib/fetchCountries.ts
+++ lib/fetchCountries.ts
@@ -1,9 +1,12 @@
 import { Country } from "@/types/country";
 
 export async function fetchCountries(): Promise<Country[]> {
-  const res = await fetch("https://restcountries.com/v3.1/all", {
-    next: { revalidate: 3600 }, // 1시간마다 캐시 재검증
-  });
+  const res = await fetch(
+    "https://restcountries.com/v3.1/independent?status=true",
+    {
+      next: { revalidate: 3600 }, // 1시간마다 캐시 재검증
+    }
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch countries");
